3181d3c7f66a51161d30541d31dcdd2eb44aeb7f
comscisau-apipat <s5752410048@sau.ac.thgit config --global user.email s5752410048@sau.ac.th>
2017-03-07 15:18:26 +0700

    update #1 naja

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<title>Document</title>
+</head>
+<body>
+	<center>
+		<h1>Home Page</h1>
+		<hr>
+		<form action="">
+		User name : <input type="text" name="uname">
+		<br><br>
+		Password : <input type="password" name="upassword">
+		<br><br>
+		<input type="submit" name="Sign In">
+		<input type="reset" name="Cancel">
+		</form>
+		<br>
+		<a href="registermember.php">ลงทะเบียน</a>
+	</center>
+	
+</body>
+</html>
